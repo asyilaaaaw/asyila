6543272f3800b61d810251d402609b9b46110e40
asyilaaaaw <asyilawaddanaf@students.untidar.ac.id>
2026-08-25 23:47:46 +0700

    Add files via upload

---
+++ identitas.py
@@ -0,0 +1,22 @@
+print(">*=_=_=_=_=_=_=_=_=_=_=_-----+***+-----_=_=_=_=_=_=_=_=_=_=_=*<")
+print("\t\t\t\t\tDATA IDENTITAS MAHASISWA")
+print(">*=_=_=_=_=_=_=_=_=_=_=_-----+***+-----_=_=_=_=_=_=_=_=_=_=_=*<")
+#menampilkan garis pembuka dan judul
+
+print("Nama\t\t\t\t\t: Asyila Wadda Nafi'ah")
+#menampilkan nama
+
+print("NIM\t\t\t\t\t\t: 2605060004")
+#menampilkan NIM
+
+print("Tempat, Tanggal Lahir\t: Kota Magelang, 9 Juli 2008")
+#menampilkan tempat dan tanggal lahir
+
+print("Jenis Kelamin\t\t\t: Perempuan")
+#menampilkan jenis kelamin
+
+print("Hobi\t\t\t\t\t: Membaca, menulis, dan jalan-jalan")
+#menampilkan hobi
+
+print("=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=_=")
+#menampilkan garis penutup
